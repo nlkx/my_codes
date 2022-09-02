a8eda2c40177dbad7601c5355db9c33a1c46d050
Nicolle <112467084+nlkx@users.noreply.github.com>
2022-09-02 20:46:41 +1200

    Add files via upload

---
+++ mycodes.py
@@ -0,0 +1,23 @@
+# Current Feels
+
+x = "Nicolle is having a "
+y = "headache"
+print(x + y)
+
+# Will Nicolle pass this assignment?
+
+txt = "Nicolle will pass this assignment!"
+if "pass" in txt:
+    print("Yes, she will 'pass'.")
+
+# What score will Nicolle get?
+
+x = 100 
+
+if x > 50:
+  print("Above fifty she will buy Roman a coffee,")
+  if x > 60:
+    print("along with a cookie!")
+  else:
+    print("Try again.")
+
